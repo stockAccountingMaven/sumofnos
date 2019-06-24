66519c51f4aac49d1f81f716e100d4c8cefcda1e
ranjith <ranjith@DESKTOP-RANJITH>
2019-06-24 09:53:48 +0530

    sum of two numbers

---
+++ src/sumoftwoNumbers/Addtionoftwonos.java
@@ -0,0 +1,26 @@
+package sumoftwoNumbers;
+
+public class Addtionoftwonos {
+
+	public static void main(String[] args) {
+		
+		
+	   Addtionoftwonos obj = new Addtionoftwonos();
+	   
+	   
+	   obj.Add(10, 20);
+		
+		
+		
+		
+
+	}
+	
+	public void Add(int a,int b)
+	{
+		
+		
+		System.out.println(a+b);
+	}
+
+}
